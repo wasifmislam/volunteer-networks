f33d0ac80aa5529c7ca85a9b211192ad5de35e83
wasifmislam <wasif.nsu@gmail.com>
2020-10-08 23:30:58 +0600

    networks site started

---
+++ src/fakeData/Vol.js
@@ -0,0 +1,124 @@
+var Vol = [
+    {
+        title: 'Children Support',
+        image: 'https://i.ibb.co/chc2PzP/child-Support.png',
+        volType: 'ChildrenSupport',
+        color: 'green'
+    },
+    {
+        title: 'Refuge Shelter',
+        image: 'https://i.ibb.co/kDj475B/refuse-Shelter.png',
+        volType: 'RefugeShelter',
+        color: 'yellow'
+    },
+    {
+        title: 'Food Charity',
+        image: 'https://i.ibb.co/NnhghPd/food-Charity.png',
+        volType: 'FoodCharity',
+        color: 'green'
+    },
+    {
+        title: 'Host a Clothing Swap',
+        image: 'https://i.ibb.co/L617fSg/cloth-Swap.png',
+        volType: 'ClothingSwap',
+        color: 'yellow'
+        
+    },
+    {
+        title: 'Host a River Clean-Up',
+        image: 'https://i.ibb.co/9yBFT4v/river-Clean.png',
+        volType: 'RiverCleanUp',
+        color: 'blue'
+    },
+    {
+        title: 'Clean Water for Children',
+        image: 'https://i.ibb.co/YN7qcjL/clean-Water.png',
+        volType: 'CleanWaterForChildren',
+        color: 'yellow'
+    },
+    {
+        title: 'Good Education',
+        image: 'https://i.ibb.co/SBwQprs/good-Education.png',
+        volType: 'GoodEducation',
+        color: 'yellowgreen'
+    },
+    {
+        title: 'New Book for Children',
+        image: 'https://i.ibb.co/L95NWKz/newBooks.png',
+        volType: 'BookForChildren',
+        color: 'red'
+    },
+    {
+        title: 'Building Bird Houses for Neighbors',
+        image: 'https://i.ibb.co/qyYvFsW/bird-House.png',
+        volType: 'BuildingBirdHouses',
+        color: 'yellowgreen'
+    },
+    {
+        title: 'Clean up Your Local Park',
+        image: 'https://i.ibb.co/jzZpqtJ/clearn-Park.png',
+        volType: 'CleanUpYourLocalPark',
+        color: 'Cerulean'
+    },
+    {
+        title: 'Organize books for the Library',
+        image: 'https://i.ibb.co/GpLHwjG/library-Books.png',
+        volType: 'OrganizeLibrary',
+        color: 'blue'
+    },
+    {
+        title: 'Give free music lessons',
+        image:  'https://i.ibb.co/NsTRJt2/music-Lessons.png',
+        volType: 'Give Free Music',
+        color: 'green'
+    },
+    {
+        title: 'Teach people how to register to vote',
+        image:  'https://i.ibb.co/tcq4S5q/vote-Register.png',
+        volType: 'Tech people How to register to vote',
+        color: 'red'
+    },
+    {
+        title: 'Humanity more volunteer activities',
+        image:  'https://i.ibb.co/Tr5yxJL/extra-Volunteer.png',
+        volType: 'Humanity activities',
+        color: 'Scarlet'
+    },
+    {
+        title: 'Foster a shelter animal',
+        image:  'https://i.ibb.co/rdfT9vj/animal-Shelter.png',
+        volType: 'shelterAnimal',
+        color: 'Tan'
+    },
+    {
+        title: 'Collect School Supplies',
+        image:  'https://i.ibb.co/nb91rFk/school-Suffiles.png',
+        volType: 'SchoolSupplies',
+        color: 'Silver'
+    },
+    {
+        title: 'Babysit during PTA meetings',
+        image:  'https://i.ibb.co/YWgn38S/babySit.png',
+        volType: 'babySit',
+        color: 'Ochre'
+    },  {
+        title: 'Collect Stuffed Animals',
+        image:  'https://i.ibb.co/b6bdW1G/stuffed-Animals.png',
+        volType: 'CollectStuffedAnimals',
+        color: 'Plum'
+    },
+    {
+        title: 'Give a Seminer on Driving Safety',
+        image:  'https://i.ibb.co/HYJwrzp/drive-Safety.png',
+        volType: 'Seminer on driving Safety',
+        color: 'Jungle green'
+    },
+    {
+        title: 'Give IT Help to Local Adults',
+        image:  'https://i.ibb.co/HGnCWK7/ITHelp.png',
+        volType: 'Give IT help',
+        color: 'Lavender'
+    }
+    
+];
+export default Vol;
